67bb026cd7481185b4d77e4e5481a32447f1a55f
Firpearce <firpearce@Firpearces-Mac-Pro.local>
2024-08-10 18:14:18 +0700

    feat: add member check

--- src/domain/member/memberService.js
+++ src/domain/member/memberService.js
@@ -1,8 +1,10 @@
 const MemberRepository = require("./memberRepository");
+const LoanRepository = require("../loan/loanRepository");
 
 class MemberService {
   constructor() {
     this.memberRepository = new MemberRepository();
+    this.loanRepository = new LoanRepository();
   }
 
   async addMember(memberInfo) {
@@ -14,7 +16,24 @@ class MemberService {
   }
 
   async findAll() {
-    return await this.memberRepository.findAll();
+    // find all members and count the number of loans for each member status_loan = Not Returned
+    const members = await this.memberRepository.findAll();
+    // Map members to include the count of loans not returned
+    const membersWithLoans = await Promise.all(
+      members.map(async (member) => {
+        const loans = await this.loanRepository.findLoanByMemberCode(
+          member.code
+        );
+        const loansNotReturned = loans.filter(
+          (loan) => loan.status_loan === "Not Returned"
+        );
+        return {
+          ...member.toObject(),
+          borrowedBook: loansNotReturned.length,
+        };
+      })
+    );
+    return await membersWithLoans;
   }
 
   async updateMember(memberCode, memberInfo) {
